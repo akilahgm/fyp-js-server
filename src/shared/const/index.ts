8d42b871dc751abfc2573844827edc6548ecfbf5
akilahgm <akilahgm@gmail.com>
2020-12-30 11:50:50 +0530

    change address

--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -1,4 +1,4 @@
-export const contractAddress = '0x18e4B52bd92D12ac09f92dC6D13a4Ecb8BDd2c96';
+export const contractAddress = '0x2BABb3ea68799Ddcb223bB008543210d8a98C8c7';
 export const contractOwner = '0xd383E9fFd9Fa749a71F7C1d4E08ed67fE00A4214';
 export const ownerPrivateKey =
   '6fa26309889904a7c4555133392b3b30627550b0fd039115ad27bdfa8f212d69';
