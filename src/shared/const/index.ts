5938f3f416df5b1182bb284ec8b38637fcfa7571
Akila K Gunawardhana <38603664+akilahgm@users.noreply.github.com>
2021-02-16 00:02:28 +0530

    update contract address

--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -1,4 +1,4 @@
-export const contractAddress = '0x2BABb3ea68799Ddcb223bB008543210d8a98C8c7';
+export const contractAddress = '0xbC750Ac25bB2dD32f050565399F7803a1BD73D55';
 export const contractOwner = '0xd383E9fFd9Fa749a71F7C1d4E08ed67fE00A4214';
 export const ownerPrivateKey =
   '6fa26309889904a7c4555133392b3b30627550b0fd039115ad27bdfa8f212d69';
