3ed66d3f48dcb9b19c73b8dace64f863b6223ad8
Akila K Gunawardhana <38603664+akilahgm@users.noreply.github.com>
2021-02-17 09:55:44 +0530

    change contract address

--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -1,4 +1,4 @@
-export const contractAddress = '0xbC750Ac25bB2dD32f050565399F7803a1BD73D55';
+export const contractAddress = '0x242ba0CE6bCB596d7607852B25D3415dA210B528';
 export const contractOwner = '0xd383E9fFd9Fa749a71F7C1d4E08ed67fE00A4214';
 export const ownerPrivateKey =
   '6fa26309889904a7c4555133392b3b30627550b0fd039115ad27bdfa8f212d69';
